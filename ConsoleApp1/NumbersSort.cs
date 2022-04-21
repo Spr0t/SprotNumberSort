1a9839d12fe6ba85e4f592549703987d1211d9f3
Serj Prots <serganas995@gmail.com>
2022-04-21 20:19:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/NumbersSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class NumbersSort
+    { 
+
+        public  List<int> Numbers { get; set; }
+        public int Count { get; set ;}
+
+        public NumbersSort(int Count)
+        {
+            this.Count = Count;
+        }
+
+
+        public void GenerateNumbers()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < Count; i++)
+            {
+                Random rnd = new Random(i);
+                int x = rnd.Next(0, 100);
+                numbers.Add(x);
+            }  
+            this.Numbers = numbers;
+        }
+        public void Sort()
+        {
+            int swap;
+
+            for (int i = 0;i <Count - 1;i++)
+            {
+                for (int j = i + 1; j < Count; j++)
+                {
+                    if (Numbers[i] > Numbers[j])
+                    {
+                        swap = Numbers[i];
+                        Numbers[i] = Numbers[j];
+                        Numbers[j] = swap;
+                    }
+                }
+            }
+        }
+
+
+        public void DrawSorted()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine($"Отсортировано : {Numbers[i]}");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите кол-во случайных чисел: ");
+            int go = int.Parse(Console.ReadLine());
+            NumbersSort n = new NumbersSort(go);
+            n.GenerateNumbers();
+            n.Sort();
+            n.DrawSorted();
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+
+
+    
+}
